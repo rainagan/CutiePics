7d03ea593b7b9849c7ba84c77e70415ce3dd5bdb
Raina <Raina@v1020-wn-67-19.campus-dynamic.uwaterloo.ca>
2018-01-25 15:25:20 -0500

    add tool page interface

--- app/src/main/java/cutiepicsuwaterloo/cutiepics/MainActivity.java
+++ app/src/main/java/cutiepicsuwaterloo/cutiepics/MainActivity.java
@@ -2,12 +2,31 @@ package cutiepicsuwaterloo.cutiepics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button camera, gallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera = (Button) findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do staff here
+            }
+        });
+
+        gallery = (Button) findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do staff here
+            }
+        });
     }
 }
---
+++ app/src/main/java/cutiepicsuwaterloo/cutiepics/ToolsActivity.java
@@ -0,0 +1,10 @@
+package cutiepicsuwaterloo.cutiepics;
+
+import android.app.Activity;
+
+/**
+ * Created by Raina on 2018-01-25.
+ */
+
+public class ToolsActivity extends Activity {
+}
