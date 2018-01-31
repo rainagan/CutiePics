7373be39d6d0f1f38ead1a61bf2875c6f1ef0c38
Raina <Raina@Rainas-MacBook-Pro.local>
2018-01-31 01:23:44 -0500

    remove redundant activities

--- app/src/main/java/ygz/cutiepics/MainActivity.java
+++ app/src/main/java/ygz/cutiepics/MainActivity.java
@@ -1,17 +1,37 @@
 package ygz.cutiepics;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private Button camera, gallery;
     private int REQUEST_CAMERA = 0;
     private final int SELECT_FILE = 1;
+    public final int PHOTO_GALLERY_REQUEST = 20;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+//                startActivity(intent);
+                cameraIntent();
             }
         });
 
@@ -34,15 +55,139 @@ public class MainActivity extends AppCompatActivity {
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
-                startActivity(intent);            }
+//                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+//                startActivity(intent);
+                galleryIntent();
+            }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    cameraIntent();
+                } else {
+                    Toast.makeText(MainActivity.this,"Taking photo permision is denined", Toast.LENGTH_SHORT);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CAMERA) {
+            if (resultCode == Activity.RESULT_OK) {
+                onCaptureImageResult(data);
+                Intent intent = new Intent(MainActivity.this, ToolsActivity.class);
+                intent.putExtra("image", mCurrentPhotoPath);
+                intent.putExtra("type", "camera");
+                startActivity(intent);
+            }
+        } else {
+            if (resultCode == Activity.RESULT_OK) {
+                // onSelectFromGalleryResult(data);
+                Uri imageUri = data.getData();
+                this.mCurrentPhotoPath = getRealPathFromURI(imageUri);
+                returnURI(this.mCurrentPhotoPath);
+            }
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onBackPressed() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // ensure that htere's a camera activity to hadle the intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                // error occurred while creating the File
+                Log.e("IOException", "unable to create image file");
+                e.printStackTrace();
+            }
+            // continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, REQUEST_CAMERA);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imgFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imgFileName, ".jpg", storageDir);
+
+        // save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+
+        Uri data = Uri.parse(pictureDirectoryPath);
+        intent.setType("image/*");
+        intent.setDataAndType(data, "image/*");
+        startActivityForResult(intent, PHOTO_GALLERY_REQUEST);
+    }
+
+    private String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+
+    public void returnURI(String uri) {
+        Intent intent = new Intent(MainActivity.this, ToolsActivity.class);
+        intent.putExtra("image", uri);
+        intent.putExtra("type", "gallery");
         startActivity(intent);
     }
 }
